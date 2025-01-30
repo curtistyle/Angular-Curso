962da58757fc143f5bbf34431d934e4bb82781d4
seb_cep_12@hotmail.com <seb_cep_12@hotmail.com>
2025-01-30 19:02:00 -0300

    create info-page of the country
    this component show the data by id from country

--- 04-countryApp/src/app/countries/pages/country-page/country-page.component.ts
+++ 04-countryApp/src/app/countries/pages/country-page/country-page.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {CountriesService} from '../../services/countries.service';
+import {switchMap} from 'rxjs';
+import {Country} from '../../interfaces/country';
+import {DecimalPipe, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-country-page',
-  imports: [],
+  imports: [
+    NgIf,
+    DecimalPipe
+  ],
   templateUrl: './country-page.component.html',
   styles: ``
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+  public country?: Country;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private countriesService: CountriesService
+  ) {  }
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap( ( { id }) =>  this.countriesService.searchCountryByAlphaCode( id ) )
+      )
+      .subscribe( country => {
+        if ( !country ){
+          return this.router.navigateByUrl( '' );
+        }
+        console.log('TENEMOS UN PAIS');
+        return this.country = country;
+      } )
+  }
+
 
 }
