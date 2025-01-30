d54592de2d197de27f4be1410ded742d75f55f34
seb_cep_12@hotmail.com <seb_cep_12@hotmail.com>
2025-01-30 18:53:28 -0300

    import component
    import countryTableComponent

--- 04-countryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ 04-countryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import {SearchBoxComponent} from '../../../shared/components/search-box/search-box.component';
 import {CountriesService} from '../../services/countries.service';
 import {Country} from '../../interfaces/country';
-import {NgForOf} from '@angular/common';
+
+import {CountryTableComponent} from '../../components/country-table/country-table.component';
 
 @Component({
   selector: 'app-by-capital-page',
   imports: [
     SearchBoxComponent,
-    NgForOf
+    CountryTableComponent
   ],
   templateUrl: './by-capital-page.component.html',
   styles: ``
