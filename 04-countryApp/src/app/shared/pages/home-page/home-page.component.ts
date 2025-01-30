278b80a67dd86d8d9e70f8d272658d576ff7693f
seb_cep_12@hotmail.com <seb_cep_12@hotmail.com>
2025-01-29 23:57:35 -0300

    create shared component

---
+++ 04-countryApp/src/app/shared/pages/home-page/home-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-home-page',
+  imports: [],
+  templateUrl: './home-page.component.html',
+  styles: ``,
+
+})
+export class HomePageComponent {
+
+}
