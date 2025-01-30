7cd389e1b7f4b6a6b985dd7766ad0540369e0ffe
seb_cep_12@hotmail.com <seb_cep_12@hotmail.com>
2025-01-30 18:55:47 -0300

    implementation;
    implementation of the methods that collect information from api.

--- 04-countryApp/src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ 04-countryApp/src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import {Country} from '../../interfaces/country';
+import {CountriesService} from '../../services/countries.service';
+import {SearchBoxComponent} from '../../../shared/components/search-box/search-box.component';
+import {CountryTableComponent} from '../../components/country-table/country-table.component';
 
 @Component({
   selector: 'app-by-country-page',
-  imports: [],
+  imports: [
+    SearchBoxComponent,
+    CountryTableComponent
+  ],
   templateUrl: './by-country-page.component.html',
   styles: ``
 })
 export class ByCountryPageComponent {
+  public countries: Country[] = new Array<Country>();
+
+  constructor( private countriesService: CountriesService ) {
+
+  }
+
+  searchByCountry(country: string): void {
+    this.countriesService.searchCountry( country )
+      .subscribe( (countries:Country[]): void => {
+        this.countries = countries
+      } )
+  };
 
 }
--- 04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ 04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import {Country} from '../../interfaces/country';
+import {CountriesService} from '../../services/countries.service';
+import {SearchBoxComponent} from '../../../shared/components/search-box/search-box.component';
+import {CountryTableComponent} from '../../components/country-table/country-table.component';
 
 @Component({
   selector: 'app-by-region-page',
-  imports: [],
+  imports: [
+    SearchBoxComponent,
+    CountryTableComponent
+  ],
   templateUrl: './by-region-page.component.html',
   styles: ``
 })
 export class ByRegionPageComponent {
+  public regions: Country[] = new Array<Country>();
 
+  constructor( private countriesService: CountriesService ) {
+
+  }
+
+  searchRegion( region: string ): void {
+    this.countriesService.searchRegion( region )
+      .subscribe( ( regions: Country[] ) => {
+        this.regions = regions;
+      } );
+  }
 }
