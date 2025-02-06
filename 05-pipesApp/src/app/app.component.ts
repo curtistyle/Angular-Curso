29ce72789e3c342ff47c601aa515c9c1459074c5
seb_cep_12@hotmail.com <seb_cep_12@hotmail.com>
2025-02-05 23:24:11 -0300

    config route and app main component

---
+++ 05-pipesApp/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { PrimeNG } from 'primeng/config';
+import { MenuComponent } from './shared/components/menu/menu.component';
+
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, MenuComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  title = 'pipesApp';
+
+  constructor(private primeng: PrimeNG) {}
+
+  ngOnInit() {
+    this.primeng.ripple.set(true);
+
+  }
+
+}
---
+++ 05-pipesApp/src/app/app.config.ts
@@ -0,0 +1,26 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+
+
+// PrimeNG
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Material from '@primeng/themes/material';
+
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Material,
+        options: { darkModeSelector: 'system' }
+      }
+    })
+
+  ]
+};
