484ca57096bbca0513e76f1a7425dbd1a43f25cf
seb_cep_12@hotmail.com <seb_cep_12@hotmail.com>
2025-02-10 22:00:03 -0300

    add global pipe language, esp.

--- 05-pipesApp/src/app/app.config.ts
+++ 05-pipesApp/src/app/app.config.ts
@@ -1,17 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-
 
 // PrimeNG
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Material from '@primeng/themes/material';
 
+// global pipe language
+import { LOCALE_ID } from '@angular/core'
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: LOCALE_ID, useValue: 'es-AR' },
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
