b310a31d3da5a85de7870ba75016bb9b500f687e
seb_cep_12@hotmail.com <seb_cep_12@hotmail.com>
2025-02-10 22:00:35 -0300

    implement basics pipes

--- 05-pipesApp/src/app/products/pages/basics-pages/basics-pages.component.ts
+++ 05-pipesApp/src/app/products/pages/basics-pages/basics-pages.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { Panel } from 'primeng/panel';
+import { Card } from 'primeng/card';
+import { DatePipe, LowerCasePipe, registerLocaleData, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import localeEsAr from '@angular/common/locales/es-AR';
+
+registerLocaleData(localeEsAr);
 
 @Component({
   selector: 'app-basics-pages',
-  imports: [],
+  imports: [
+    Panel,
+    Card,
+    UpperCasePipe,
+    LowerCasePipe,
+    TitleCasePipe,
+    DatePipe
+  ],
   templateUrl: './basics-pages.component.html',
   styleUrl: './basics-pages.component.css'
 })
 export class BasicsPagesComponent {
+  public nameLower: string = 'curtis';
+  public nameUpper: string = 'Curtis';
+  public fullName: string = 'cURtIs sTYle';
 
+  public customDate: Date = new Date();
 }
