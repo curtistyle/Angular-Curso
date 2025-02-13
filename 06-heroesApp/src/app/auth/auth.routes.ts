9c8b27b93078ce628091efc0be59d62430a995a1
seb_cep_12@hotmail.com <seb_cep_12@hotmail.com>
2025-02-13 20:40:55 -0300

    create all routes
    create all routes with their respective children routes

---
+++ 06-heroesApp/src/app/auth/auth.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      { path: 'login', component: LoginPageComponent },
+      { path: 'new-account', component: RegisterPageComponent },
+      { path: '**', redirectTo: 'login' }
+    ]
+  },
+
+]
