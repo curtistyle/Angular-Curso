7eeda74a50f3914de09430852b845eedcaee3b97
seb_cep_12@hotmail.com <seb_cep_12@hotmail.com>
2025-02-20 15:41:32 -0300

    add guards in routes

--- 06-heroesApp/src/app/app.routes.ts
+++ 06-heroesApp/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
+import { AuthGuard } from './auth/guards/auth.guard';
+import { PublicGuard } from './auth/guards/public.guard';
 
 export const routes: Routes = [
-  { path: 'auth', loadChildren: () => import('./auth/auth.routes').then(r => r.routes) },
-  { path: 'heroes', loadChildren: () => import('./heroes/heroes.routes').then(r => r.routes) },
+  { path: 'auth', loadChildren: () => import('./auth/auth.routes').then(r => r.routes), canActivate: [ PublicGuard ], canMatch: [ PublicGuard ] },
+  { path: 'heroes', loadChildren: () => import('./heroes/heroes.routes').then(r => r.routes), canActivate: [ AuthGuard ], canMatch: [ AuthGuard ] },
   { path: '404', component: Error404PageComponent },
   { path: '', redirectTo: 'heroes', pathMatch: 'full' },
   { path: '**', redirectTo: '404' }
