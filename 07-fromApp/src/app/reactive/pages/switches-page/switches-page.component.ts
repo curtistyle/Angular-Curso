14fcf371d3847446baf1b6adf6767bbdc870f2fb
seb_cep_12@hotmail.com <seb_cep_12@hotmail.com>
2025-02-23 23:21:05 -0300

    another Forms

---
+++ 07-fromApp/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -0,0 +1,51 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { JsonPipe, NgIf } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-switches-page',
+  imports: [
+    JsonPipe,
+    ReactiveFormsModule,
+    NgIf
+  ],
+  templateUrl: './switches-page.component.html',
+  styleUrl: './switches-page.component.css'
+})
+export class SwitchesPageComponent implements OnInit{
+  private fb = inject(FormBuilder)
+
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotification: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue]
+  });
+
+  public person = {
+    gender: 'F',
+    wantNotifications: false
+  }
+
+  public ngOnInit(): void {
+    this.myForm.reset( this.person );
+  }
+
+  public onSave() {
+    if ( this.myForm.invalid ){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    // crea un nuevo objeto (newPerson) sin la propiedad tersAndConditions de this.myForm.value
+    const { termsAndConditions, ...newPerson } = this.myForm.value;
+
+    this.person = newPerson;
+    console.log(this.myForm.value);
+    console.log(this.person)
+  }
+
+  public isValidField( field: string) {
+    const control = this.myForm.get(field) as FormControl;
+    return control?.errors && control.touched;
+  }
+}
